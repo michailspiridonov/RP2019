af8d8d38ab30fc3bea5697af360ac33ce6b6035b
Coconut1233 <spiridonov.michail1@gmail.com>
2020-02-22 17:37:17 +0100

    created separate folder for pages

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var express=require('express');
-var app=express();
-app.get('/',function(req,res)
-{
-res.send('Hello World!');
+var express = require('express');
+var app = express();
+const pagesPath = "/home/michail/RP2019/pages/";
+app.get('/', function (req, res) {
+    res.sendFile(pagesPath+'index.html', {root : '/'});
 });
-var server=app.listen(3000,function() {});
+var server = app.listen(3000, function () { });
